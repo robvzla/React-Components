7db4a88a0f10db8848a348f7102b70690ebc33a6
roberto cassino <robertocassino@student.griffith.ie>
2023-08-15 10:11:59 +0100

    Adding the nav bar to the app

--- learning-react-components/src/App.js
+++ learning-react-components/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <NavBar />
+      {/* <h1>Hello</h1> */}
+      {/* NavBar goes here */}
     </div>
   );
 }
